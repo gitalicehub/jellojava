1abcb6f6b91d297e9e5b3a963938d093290c293e
gitalicehub <tazaspice@gmail.com>
2018-04-29 22:26:15 +0530

    create hellocalc.java

---
+++ hellocalc.java
@@ -0,0 +1,46 @@
+/**
+ * JavaDoc comments look like this. Used to describe the Class or various
+ * attributes of a Class.
+ * Main attributes:
+ *
+ * @author         Name (and contact information such as email) of author(s).
+ * @version     Current version of the program.
+ * @since        When this part of the program was first added.
+ * @param         For describing the different parameters for a method.
+ * @return        For describing what the method returns.
+ * @deprecated  For showing the code is outdated or shouldn't be used.
+ * @see         Links to another part of documentation.
+*/
+
+// Import ArrayList class inside of the java.util package
+import java.util.ArrayList;
+// Import all classes inside of java.security package
+import java.security.*;
+
+public class LearnJava {
+
+    // In order to run a java program, it must have a main method as an entry
+    // point.
+    public static void main(String[] args) {
+
+    ///////////////////////////////////////
+    // Input/Output
+    ///////////////////////////////////////
+
+        /*
+        * Output
+        */
+
+        // Use System.out.println() to print lines.
+        System.out.println("Hello World!");
+        System.out.println(
+            "Integer: " + 10 +
+            " Double: " + 3.14 +
+            " Boolean: " + true);
+
+        // To print without a newline, use System.out.print().
+        System.out.print("Hello ");
+        System.out.print("World");
+    } // End main method
+} // End LearnJava class
+       
